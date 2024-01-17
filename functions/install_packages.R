d95b0f75b1151bc8f56bca81486866dd5dd555cf
Hair Parra <jair.parra@outlook.com>
2024-01-16 20:14:49 -0500

    updated packages

---
+++ functions/install_packages.R
@@ -0,0 +1,44 @@
+################################################################################
+# install_packages.R
+# 
+# @author: Hair Albeiro Parra Barrera
+# 
+# This R script is designed to load specified packages and install them if they 
+# are not already installed. 
+################################################################################
+
+####################
+### 1. Functions ###
+####################
+
+# Load required packages
+f_load_packages <- function() {
+  
+  # List of packages to automatically install
+  proj_packages <- c(
+    "sf", 
+    "here", 
+    "tidyr", 
+    "dplyr", 
+    "tibble", 
+    "readxl", 
+    "corrplot", 
+    "ggplot2", 
+    "data.table", 
+    "sqldf", 
+    "car", 
+    "glmnet",
+    "elasticnet", 
+    "factoextra"
+  )
+  
+  for (package in proj_packages) {
+    # Check if the package is already installed
+    if (!requireNamespace(package, quietly = TRUE)) {
+      # If not installed, install the package with dependencies
+      install.packages(package, dependencies = TRUE)
+    }
+    # Load the package
+    library(package, character.only = TRUE)
+  }
+}
